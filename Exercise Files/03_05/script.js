7473f32e0e1c5815379e1aed5a439cc1f5a53ee7
Lotte Carleer <lottecarleer@LaptopvanLotte.lan>
2025-02-07 17:48:06 +0100

    oef 3.5 aangepast

--- Exercise Files/03_05/script.js
+++ Exercise Files/03_05/script.js
@@ -1,11 +1,11 @@
 var a = 5;
-var b = 5;
-var theNumbersMatch
+var b = "5";
+var theNumbersMatch;
 
-if ( a == b ) {
-    theNumbersMatch = true;
+if (a == b) {
+  theNumbersMatch = true;
 } else {
-    theNumbersMatch = false;
+  theNumbersMatch = false;
 }
 
-console.log(theNumbersMatch);
+console.log("theNumbersMatch:" + theNumbersMatch);
