8a386804568f1634f590f6479a24ab88e6ae3c4d
Lotte Carleer <lottecarleer@LaptopvanLotte.lan>
2025-02-23 15:48:12 +0100

    test

--- topics/dom.starter.js
+++ topics/dom.starter.js
@@ -12,10 +12,11 @@ ______ ________  ___
 // select the second paragraph and figure out a way to make the backround color yellow
 // hint: use the style property and .backgroundColor
 // hint: use the querySelector or getElementById function
-
+document.getElementById("paragraph2").style.backgroundColor = "yellow";
 // 2 - select the element with id #paragraph3 and change the text to "Never gonna run around and desert you"
 // hint: use the innerHTML property
-
+document.getElementById("paragraph3").innerHTML =
+  "Never gonna run around and desert you";
 // 3 - get multiple elements by class
 // select all the span elements by their classname and log the result to your console, what type of object is it?
 // next, loop over the elements and change the innerHTML to another emoji
